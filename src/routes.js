1fed01a1d39b62efd09fa73fa4cb32d15e0c88e7
Thayna Rodrigues <thna.rdg@gmail.com>
2022-06-29 20:02:01 -0300

    Modificando a ordem das rotas

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ const controller = require('./app/controllers/LegendaryController');
 
 const routes = new Router();
 
+routes.get('/legendaries', controller.ListData);
 routes.get('/legendaries', controller.index);
-routes.get('legendaries', controller.ListData);
 
 module.exports = routes;
