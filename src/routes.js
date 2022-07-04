60227b5806360858392798edbe20dff806a7f858
Thayna Rodrigues <thna.rdg@gmail.com>
2022-07-04 20:19:21 -0300

    Remove index

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,16 @@
 const Router = require('express');
 
 const controller = require('./app/controllers/LegendaryController');
+const uploadFileController = require('./app/controllers/UploadFileController');
+
+const app = require('./app');
 
 const routes = new Router();
 
 routes.get('/legendaries', controller.ListData);
-routes.get('/legendaries', controller.index);
 routes.post('/legendaries', controller.create);
 routes.put('/legendaries/:id', controller.update);
 routes.delete('/legendaries/:id', controller.delete);
+routes.post('/uploads', app.uploadFile.single('file'), uploadFileController.storeFile);
 
 module.exports = routes;
