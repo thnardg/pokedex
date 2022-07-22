35baa17c4880566a82beea5799be33389a9b6413
Thayna Rodrigues <thna.rdg@gmail.com>
2022-07-22 08:43:30 -0300

    Added trainer validator

--- src/middlewares/TrainerValidator.js
+++ src/middlewares/TrainerValidator.js
@@ -7,18 +7,18 @@ async function trainerValidator(request, response, next) {
       .strict()
       .required("Name é obrigatório")
       .min(10)
-      .typeError("O campo nome, deve ser uma string"),
+      .typeError("O campo nome deve ser uma string"),
     age: yup
       .number()
       .positive()
       .integer()
-      .typeError("O campo idade, deve ser um tipo inteiro"),
+      .typeError("O campo idade deve ser um tipo inteiro"),
     city: yup
       .string()
       .strict()
       .required("City é obrigatório")
       .min(10)
-      .typeError("O campo city, deve ser uma string"),
+      .typeError("O campo city deve ser uma string"),
   });
 
   await schema.validate(request.body).catch((err) => {
